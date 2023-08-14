25c8f01e117d245646fee9c02f60019705fa93db
Yusuf Emir Tatlı <yusufemirtatli96@gmail.com>
2023-08-14 18:02:35 +0300

    gün1 son

---
+++ src/app/movie.datasource.ts
@@ -0,0 +1,7 @@
+import {Movie} from "./movie";
+
+export const MoviesList:Movie[]=[
+  {id:1,name:"Emir Film"},
+  {id:2,name:"Emir Film2"},
+  {id:3,name:"Emir Film3"}
+]
--- src/app/movies/movies.components.ts
+++ src/app/movies/movies.components.ts
@@ -1,9 +1,20 @@
 import {Component} from "@angular/core";
+import {Movie} from "../movie";
+import {MoviesList} from "../movie.datasource";
+import {MovieComponent} from "../movie/movie.component";
 
 @Component({
   selector: 'movies',
-  templateUrl: 'movies.component.html'
+  templateUrl: 'movies.component.html',
+  styleUrls:['movies.component.css']
 })
 export class MoviesComponents {
+  title="Movie Name";
+  movies=MoviesList;
+  selectedMovie: Movie | any;
+    onSelect(movie:Movie): void{
+      this.selectedMovie=movie;
+}
 
+  protected readonly Movie = Movie;
 }
