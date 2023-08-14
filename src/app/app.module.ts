337b3ad6ff7d141fd3d15e8a7fb0cf55bca58ca2
Yusuf Emir Tatlı <yusufemirtatli96@gmail.com>
2023-08-14 15:16:42 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MoviesComponents} from "./movies/movies.components";
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponents,
+    MovieComponent
   ],
   imports: [
     BrowserModule
